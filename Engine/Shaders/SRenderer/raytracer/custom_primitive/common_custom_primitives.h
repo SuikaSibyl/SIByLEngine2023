0cc6784076fdaa8c9093c653a6dd4b2069c078d8
Suika <60009633+SuikaSibyl@users.noreply.github.com>
2023-01-11 09:32:07 -0800

    SRenderer :: add robust ray-sphere intersection with small radius

--- Engine/Shaders/SRenderer/raytracer/custom_primitive/common_custom_primitives.h
+++ Engine/Shaders/SRenderer/raytracer/custom_primitive/common_custom_primitives.h
@@ -6,8 +6,10 @@
 
 /** 
 * Compute intersection of a ray and a sphere 
-* @ref: http://viclw17.github.io/2018/07/16/raytracing-ray-sphere-intersection 
+* @ref: http://viclw17.github.io/2018/07/16/raytracing-ray-sphere-intersection
+* @ref: <ray tracing gems> https://link.springer.com/content/pdf/10.1007/978-1-4842-4427-2_7.pdf
 */
+
 float hitSphere(
     in vec3 center, in float radius, 
     in vec3 origin, in vec3 direction)
@@ -16,9 +18,25 @@ float hitSphere(
     const float a            = dot(direction, direction);
     const float b            = 2.0 * dot(oc, direction);
     const float c            = dot(oc, oc) - radius * radius;
+    // Degenerated case
+    if (a == 0) {
+        if (b == 0)
+            return -1.0; // if no solve
+        return -c / b;
+    }
+    vec3 term = oc - (dot(oc,direction)*direction);
+    float discriminant = 4* a * (radius*radius - dot(term,term));
+    if (discriminant < 0)
+        return -1.0; // if no solve
+    float root_discriminant = sqrt(discriminant);
     float t0, t1;
-    bool itersected = quadratic(a, b, c, t0, t1);
-    if(!itersected) return -1.0; // if no solve
+    if (b >= 0) {
+        t0 = (- b - root_discriminant) / (2 * a);
+        t1 = 2 * c / (- b - root_discriminant);
+    } else {
+        t0 = 2 * c / (- b + root_discriminant);
+        t1 = (- b + root_discriminant) / (2 * a);
+    }
     float t = -1;
     if (t0 >= 0 && t0 < T_MAX)
         t = t0;
